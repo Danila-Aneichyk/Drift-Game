37545c3b648d44b54b048d69bb32a378321438f1
Анейчик Данила <aneychik@awem.com>
2024-05-07 18:19:56 +0300

    Добавил экран окончания игры.

---
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Timer = DriftSystem.Timer;
+
+namespace UI
+{
+    public class GameOverScreen : MonoBehaviour
+    {
+        [SerializeField] private GameObject _gameOverPanel;
+        [SerializeField] private Timer _timer;
+
+        private void Start()
+        {
+            _timer.TimerExpired += OnTimerExpired;
+        }
+
+        private void OnTimerExpired()
+        {
+            _gameOverPanel.SetActive(true);
+        }
+    }
+}
